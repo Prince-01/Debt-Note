e18a5a0d0c64e2db67b59f1b17bcd02f93438b73
Prince-01 <212798@student.pwr.edu.pl>
2015-08-15 22:23:33 +0200

    Removed unnecessary files, and developed look-in feature in debt class algorithm

--- src/com/company/Debt.java
+++ src/com/company/Debt.java
@@ -15,6 +15,26 @@ public class Debt {
     private List<Pair<Date, Double>> debtPercentage = new ArrayList<>();
     private List<Pair<Date, Integer>> debtRecurrence = new ArrayList<>();
 
+    class CalcInfo {
+        Date date;
+        double value;
+        MODIFICATIONS modification;
+
+        public CalcInfo(Date d, double v, MODIFICATIONS mod) {
+        date = d;
+        value = v;
+        modification = mod;
+        }
+
+        double perform(double in) {
+            if(modification == MODIFICATIONS.INCREASE_ADDITION)
+                return value + in;
+            else if(modification == MODIFICATIONS.INCREASE_PERCENTAGE)
+                return in * (1 + value / 100);
+            return -1;
+        }
+    }
+
     enum MODIFICATIONS {
         INCREASE_ADDITION,
         INCREASE_PERCENTAGE,
@@ -36,8 +56,8 @@ public class Debt {
         this.initialDebt = initialDebt;
     }
 
-    public double calculateDebt(Date limit) {
-        double result = initialDebt;
+    public List<CalcInfo> calculateInSteps(Date limit) {
+        List<CalcInfo> result = new ArrayList<>();
 
         if(debtPercentage.size() != 0) {
             Date d = convDate(debtPercentage.get(0).getKey());
@@ -47,30 +67,40 @@ public class Debt {
 
             while (d.compareTo(limit) <= 0) {
                 while (incrCntg < increases.size() && d.compareTo(convDate(increases.get(incrCntg).getKey())) == 1)
-                    result += increases.get(incrCntg++).getValue();
+                    result.add(new CalcInfo(convDate(increases.get(incrCntg).getKey()), increases.get(incrCntg++).getValue(), MODIFICATIONS.INCREASE_ADDITION));
                 if (percCntg < debtPercentage.size() - 1) {
                     if (d.compareTo(convDate(debtPercentage.get(percCntg).getKey())) >= 0 && d.compareTo(convDate(debtPercentage.get(percCntg + 1).getKey())) < 1)
-                        result *= (1 + debtPercentage.get(percCntg).getValue() / 100);
+                        result.add(new CalcInfo(d, debtPercentage.get(percCntg).getValue(), MODIFICATIONS.INCREASE_PERCENTAGE));
                     else
-                        result *= (1 + debtPercentage.get(++percCntg).getValue() / 100);
+                        result.add(new CalcInfo(d, debtPercentage.get(++percCntg).getValue(), MODIFICATIONS.INCREASE_PERCENTAGE));
                 } else {
-                    result *= (1 + debtPercentage.get(percCntg).getValue() / 100);
+                    result.add(new CalcInfo(d, debtPercentage.get(percCntg).getValue(), MODIFICATIONS.INCREASE_PERCENTAGE));
                 }
                 while (incrCntg < increases.size() && d.compareTo(convDate(increases.get(incrCntg).getKey())) == 0)
-                    result += increases.get(incrCntg++).getValue();
+                    result.add(new CalcInfo(convDate(increases.get(incrCntg).getKey()), increases.get(incrCntg++).getValue(), MODIFICATIONS.INCREASE_ADDITION));
 
                 while (recuCntg < debtRecurrence.size() - 1)
                     if (!(d.compareTo(convDate(debtRecurrence.get(recuCntg).getKey())) >= 0 && d.compareTo(convDate(debtRecurrence.get(recuCntg + 1).getKey())) < 1))
-                        recuCntg++;
+                        result.add(new CalcInfo(convDate(debtRecurrence.get(recuCntg).getKey()), debtRecurrence.get(recuCntg++).getValue(), MODIFICATIONS.INCREASE_ADDITION));
 
                 d.setTime(d.getTime() + 1000 * 3600 * 24 * debtRecurrence.get(recuCntg).getValue());
             }
         } else {
             for(Pair<Date, Double> d : increases)
-                result += d.getValue();
+                result.add(new CalcInfo(d.getKey(), d.getValue(), MODIFICATIONS.INCREASE_ADDITION));
         }
 
         return result;
+    }
+
+    public double calculateDebt(Date limit) {
+        List<CalcInfo> out = calculateInSteps(limit);
+        double outf = initialDebt;
+
+        for(CalcInfo cinfo : out)
+            outf = cinfo.perform(outf);
+
+        return outf;
     }
 
     public void increaseNow(double increase) {
